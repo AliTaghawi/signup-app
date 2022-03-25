b1e6fff13307658105adb6758f357f28afb21fc8
Ali Taghawi <marshal.taghawi@gmail.com>
2022-03-26 04:29:43 +0430

    useing validation and toastify in singup

--- src/components/SingUp.js
+++ src/components/SingUp.js
@@ -1,4 +1,7 @@
 import React,{useState, useEffect} from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import validate from './validate';
 
 const SingUp = () => {
@@ -9,13 +12,13 @@ const SingUp = () => {
     confirmPassword: "",
     isAccepted: false
   })
-
   const [errors, setErrors] = useState({})
+  const [touched, setTouched] = useState({})
 
   useEffect(()=> {
     setErrors(validate(state))
     console.log(errors)
-  }, [state])
+  }, [state, touched])
   
   const changHandelr = (event) => {
     if (event.target.name === "isAccepted") {
@@ -25,35 +28,62 @@ const SingUp = () => {
     }
   }
 
+  const focusHandeler = event => {
+    setTouched({...touched, [event.target.name]: true})
+  }
+
+  const submitHandeler = event => {
+    event.preventDefault();
+    if (!Object.keys(errors).length) {
+      console.log(state);
+      toast.success('You signed up successfully');
+    } else {
+      setTouched({
+        name: true,
+        email: true,
+        password: true,
+        confirmPassword: true,
+        isAccepted: true
+      });
+      toast.error('Invalid data!!!');
+    }
+  }
+
   return (
     <div>
       <form>
         <h2>Sign Up</h2>
         <div>
           <label>Name:</label>
-          <input type="text" name="name" value={state.name} onChange={changHandelr} />
+          <input type="text" name="name" value={state.name} onChange={changHandelr} onFocus={focusHandeler} />
+          {touched.name && errors.name && <span>{errors.name}</span>}
         </div>
         <div>
-          <label>Email::</label>
-          <input type="text" name="email" value={state.email} onChange={changHandelr} />
+          <label>Email:</label>
+          <input type="text" name="email" value={state.email} onChange={changHandelr} onFocus={focusHandeler} />
+          {touched.email && errors.email && <span>{errors.email}</span>}
         </div>
         <div>
           <label>Password:</label>
-          <input type="password" name="password" value={state.password} onChange={changHandelr} />
+          <input type="password" name="password" value={state.password} onChange={changHandelr} onFocus={focusHandeler} />
+          {touched.password && errors.password && <span>{errors.password}</span>}
         </div>
         <div>
           <label>Confirm Password:</label>
-          <input type="password" name="confirmPassword" value={state.confirmPassword} onChange={changHandelr} />
+          <input type="password" name="confirmPassword" value={state.confirmPassword} onChange={changHandelr} onFocus={focusHandeler} />
+          {touched.confirmPassword && errors.confirmPassword && <span>{errors.confirmPassword}</span>}
         </div>
         <div>
           <label>I accepte all tirms:</label>
-          <input type="checkbox" name="isAccepted" value={state.isAccepted} onChange={changHandelr} />
+          <input type="checkbox" name="isAccepted" value={state.isAccepted} onChange={changHandelr} onFocus={focusHandeler} />
+          {touched.isAccepted && errors.isAccepted && <span>{errors.isAccepted}</span>}
         </div>
         <div>
           <a href='##'>Login</a>
-          <button type='submit' >Creat</button>
+          <button type='submit' onClick={submitHandeler} >Submit</button>
         </div>
       </form>
+      <ToastContainer />
     </div>
   );
 };
