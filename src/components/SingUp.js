924ddcbe886dbda0214d5a02043e8dbb6ff6971e
Ali Mehrjoo <soheilmehrjoo1379@gmail.com>
2022-03-25 18:08:55 +0430

    added some styles to form

--- src/components/SingUp.js
+++ src/components/SingUp.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const SingUp = () => {
   const [state, setState] = useState({
@@ -6,45 +7,77 @@ const SingUp = () => {
     email: "",
     password: "",
     confirmPassword: "",
-    isAccepted: false
-  })
-  
+    isAccepted: false,
+  });
+
   const changHandelr = (event) => {
     if (event.target.name === "isAccepted") {
-      setState({...state, [event.target.name]: event.target.checked})
+      setState({ ...state, [event.target.name]: event.target.checked });
     } else {
-      setState({...state, [event.target.name]: event.target.value})
+      setState({ ...state, [event.target.name]: event.target.value });
     }
-    console.log(state)
-  }
+    console.log(state);
+  };
 
   return (
-    <div>
-      <form>
+    <div style={{ display: "inline-block", width: "350px" }}>
+      <form className="p-3">
         <h2>Sign Up</h2>
-        <div>
+        <div className="form-group pt-2">
           <label>Name:</label>
-          <input type="text" name="name" value={state.name} onChange={changHandelr} />
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={changHandelr}
+            className="form-control"
+          />
         </div>
-        <div>
+        <div className="form-group pt-2">
           <label>Email::</label>
-          <input type="text" name="email" value={state.email} onChange={changHandelr} />
+          <input
+            type="text"
+            name="email"
+            value={state.email}
+            onChange={changHandelr}
+            className="form-control"
+          />
         </div>
-        <div>
+        <div className="form-group pt-2">
           <label>Password:</label>
-          <input type="password" name="password" value={state.password} onChange={changHandelr} />
+          <input
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={changHandelr}
+            className="form-control"
+          />
         </div>
-        <div>
+        <div className="form-group pt-2">
           <label>Confirm Password:</label>
-          <input type="password" name="confirmPassword" value={state.confirmPassword} onChange={changHandelr} />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={state.confirmPassword}
+            onChange={changHandelr}
+            className="form-control"
+          />
         </div>
-        <div>
-          <label>I accepte all tirms:</label>
-          <input type="checkbox" name="isAccepted" value={state.isAccepted} onChange={changHandelr} />
+        <div className="form-group pt-2">
+          <input
+            type="checkbox"
+            name="isAccepted"
+            value={state.isAccepted}
+            onChange={changHandelr}
+            className="form-check-input"
+          />
+          <label className="form-check-label">I accepte all terms</label>
         </div>
-        <div>
-          <a href='##'>Login</a>
-          <button type='submit' >Creat</button>
+        <div className="d-flex justify-content-around pt-2">
+          <a href="##">Login</a>
+          <button type="submit" className="btn btn-primary">
+            Creat
+          </button>
         </div>
       </form>
     </div>
