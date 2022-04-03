1fc245c288e8b33b187b073490407a21b1595c01
Soheil2700 <soheilmehrjoo1379@gmail.com>
2022-04-03 10:15:10 +0430

    Some bugs fixed

--- src/components/SingUp.js
+++ src/components/SingUp.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { ToastContainer, toast } from "react-toastify";
 import { useFormik } from "formik";
 import * as Yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
